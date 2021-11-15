71e2de80853a36bc186e30cd0c3077a03416e8de
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-15 17:09:16 +0500

    Added: Function Description

--- textToSpeech.py
+++ textToSpeech.py
@@ -1,6 +1,15 @@
 import speech_recognition as sr
 
 def speak(engine):
+    """[It recognizes the speech and converts into the text.
+    It takes in the pyttsx3 engine to speak out the errors if encountered.]
+
+    Args:
+        engine ([pyttsx3.Engine]): [For the system to speak out the information or alerts]
+
+    Returns:
+        [string]: [The text of speech understood by the google API]
+    """
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
@@ -22,6 +31,17 @@ def speak(engine):
             engine.runAndWait()
             
 def IfIn(words, text, opr="or"):
+    """[It take a array of words and checks if the words are present in the text
+    and return their AND or OR relation mentioned in the arguments.]
+
+    Args:
+        words ([list of strings]): [The words to check if present in text]
+        text ([string]): [The simple string text]
+        opr (str, optional): [The logical realtion of booleans i.e. AND, OR]. Defaults to "or".
+
+    Returns:
+        [boolean]: [The relation of 'opr' with all the boolean of matches (word in text)]
+    """
     if opr=="or":
         for word in words:
             if word in text:
