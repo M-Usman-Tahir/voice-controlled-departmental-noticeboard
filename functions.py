8ddbb5806753dac8d366bb6a69c9b98edb19d1ad
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-18 16:27:35 +0500

    Updated: Paths Set for Modular imports

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+import SysPaths
 from textToSpeech import * # imports speech_recognition as sr
 from BOOT import * # imports pandas as pd, os, PasswordGenerator
 from BootMails import * # imports SendmMails
