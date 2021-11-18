dd0bcf407b7580117c03006388fa38913adb89c8
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-18 16:25:40 +0500

    Updated: functions (Modular) Node

--- functions.py
+++ functions.py
@@ -1,8 +1,3 @@
-import sys
-
 from textToSpeech import * # imports speech_recognition as sr
 from BOOT import * # imports pandas as pd, os, PasswordGenerator
-
-sys.path.insert(1, os.path.join(sys.path[0], "Mails"))
-
 from BootMails import * # imports SendmMails
