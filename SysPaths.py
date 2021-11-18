beda360efdc6c5d22bc376c954f34159ea38c7f5
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-18 16:32:02 +0500

    Added: Description

--- SysPaths.py
+++ SysPaths.py
@@ -1,3 +1,7 @@
+    """[Gives path to the system for the modules
+        to import other modules or files efficiently]
+    """
+
 import sys
 import os
 sys.path.insert(1, os.path.join(sys.path[0], "Mails"))
