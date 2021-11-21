16bf30772d6c827f46975d8fc12d38d3babb959b
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-21 23:35:26 +0500

    Updates: Functions Set

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@
         Takes all funstions here for the main file]
     """
 
-import SysPaths
+from SysPaths import *
 from textToSpeech import * # imports speech_recognition as sr
 from BOOT import * # imports pandas as pd, os, PasswordGenerator
 from BootMails import * # imports SendmMails
