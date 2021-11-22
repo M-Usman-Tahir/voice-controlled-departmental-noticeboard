ad5945d5a9a99deeae79dbc789d9cfcab369e1c5
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-22 07:42:01 +0500

    Added: Login Functions

--- functions.py
+++ functions.py
@@ -6,3 +6,4 @@ from SysPaths import *
 from textToSpeech import * # imports speech_recognition as sr
 from BOOT import * # imports pandas as pd, os, PasswordGenerator
 from BootMails import * # imports SendmMails
+from Login import *
