57bf115b9bb7004d0a83c17caf5abf0f19521207
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-22 07:53:00 +0500

    Updated: System Paths

--- SysPaths.py
+++ SysPaths.py
@@ -4,7 +4,11 @@
 
 import sys
 import os
+
 sys.path.insert(1, os.path.join(sys.path[0], "Mails"))
 sys.path.insert(1, os.path.join(sys.path[0], "Authentication"))
 TemplatesPath = os.path.join(sys.path[0], "templates")
-
+PublicPath = os.path.join(sys.path[0], "LOCAL", "Department-Public")
+OpportunityPath = os.path.join(sys.path[0], "LOCAL", "Opportunities")
+FacultyPath = os.path.join(sys.path[0], "LOCAL", "Faculty")
+StudentPath = os.path.join(sys.path[0], "LOCAL", "Student")
