b9092f29431c1a3701ad017f336b362426c2c551
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-24 23:58:49 +0500

    Added: Main Dir Path

--- SysPaths.py
+++ SysPaths.py
@@ -7,6 +7,7 @@ import os
 
 sys.path.insert(1, os.path.join(sys.path[0], "Mails"))
 sys.path.insert(1, os.path.join(sys.path[0], "Authentication"))
+MainDirPath = sys.path[0]
 TemplatesPath = os.path.join(sys.path[0], "templates")
 PublicPath = os.path.join(sys.path[0], "LOCAL", "Department-Public")
 OpportunityPath = os.path.join(sys.path[0], "LOCAL", "Opportunities")
