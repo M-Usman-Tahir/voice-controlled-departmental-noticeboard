993e899677cf04ca46ffdaf2d333da2609e53b21
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-29 03:49:17 +0500

    Added: Voice actions

--- init.py
+++ init.py
@@ -7,6 +7,30 @@ from functions import *
     # OpenFile
 # }
 
-Boot()
+# LogIN("2019-CE-34")
+
+# Boot()
 # print("Opening the public Notifications...")
 # OPEN(os.path.join("Notis", "Noti.jpeg"))
+
+import pyttsx3
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+def Start():
+    engine.say("Waiting for you command!")
+    engine.runAndWait()
+    # text = speak(engine)
+    text = "scan"
+    print(text)
+    Command(engine, text)
+    
+if __name__ == '__main__':
+    while True:
+        c = input("Enter space to start or any other character to exit: ")
+        if c == " ":
+            Start()
+        else:
+            exit()
