d0096dca50298f330bf4f9cb6776bb95d59245b7
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-29 03:43:15 +0500

    Added: Authentication Path

--- SysPaths.py
+++ SysPaths.py
@@ -7,12 +7,13 @@ import os
 
 sys.path.insert(1, os.path.join(sys.path[0], "Mails"))
 sys.path.insert(1, os.path.join(sys.path[0], "Authentication"))
+AuthPath = sys.path[1]
 MainDirPath = sys.path[0]
 TemplatesPath = os.path.join(sys.path[0], "templates")
 LocalPath = os.path.join(sys.path[0], "LOCAL")
 PublicPath = os.path.join(LocalPath, "Department-Public")
 DepartmentPath = os.path.join(PublicPath, "Department")
-SocietyPath = os.path,join(PublicPath, "Societies")
+SocietyPath = os.path.join(PublicPath, "Societies")
 OpportunityPath = os.path.join(LocalPath, "Opportunities")
 FacultyPath = os.path.join(LocalPath, "Faculty")
 StudentPath = os.path.join(LocalPath, "Student")
