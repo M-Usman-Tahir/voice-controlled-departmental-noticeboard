36a780c4655b283379e45ff242e29d75981dcff2
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-29 03:48:38 +0500

    Updated: Paths to modules

--- functions.py
+++ functions.py
@@ -2,15 +2,13 @@
     Takes all funstions here for the main file]
 """
 
-# from Authentication.OpenFile import OPEN
 from SetNoti import *
-from SysPaths import *
 from textToSpeech import * # imports speech_recognition as sr
 from BOOT import * # imports pandas as pd, os, PasswordGenerator
 from BootMails import * # imports SendmMails
 from Login import *
 from OpenFile import *
-from FaceDetection import *
+from FaceDetection import * # import SysPaths
 
 
 def Command(engine, text):
