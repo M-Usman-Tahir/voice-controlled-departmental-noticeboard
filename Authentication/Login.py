53529f24405a1088ca15b9a252836b5eab530351
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-11-29 03:48:00 +0500

    Added: Login Functionalities

--- Authentication/Login.py
+++ Authentication/Login.py
@@ -15,17 +15,37 @@ def FunList(List):
 def LogIN(name):
     if "CE" in name:
         path = os.path.join(SP.StudentPath, "Session-{}".format(name.split("-")[0]), name)
-        dirs = os.listdir(path)
-        dirs.remove("CREs.txt")
-        if len(dirs)>0:
+    else:
+        path = os.path.join(SP.FacultyPath, name)
+    dirs = os.listdir(path)
+    dirs.remove("CREs.txt")
+    if len(dirs)>0:
+        while True:
             print(FunList(dirs))
-            N = int(input("Enter the Number of notification to see: "))
+            print("Enter the Number of notification to see: ")
+            TEXT = speak(engine)
+            N=getNum(TEXT)
+            if N == 0:
+                break
             OPEN(os.path.join(path, dirs[N-1]))
-        else:
-            print("There are no notifications yet.")
-            
-def logout():
-    pass
+    else:
+        print("There are no notifications yet.")
+
+
+numbers = {"one":1,
+           "two":2,
+           "three":3,
+           "four":4,
+           "five":5,
+           "six":6,
+           "seven":7,
+           "eight":8,
+           "nine":9}
+
+def getNum(text):
+    for i in numbers:
+        if i in text:
+            return i
 
 if __name__ == '__main__':
     LogIN("2019-CE-34")
