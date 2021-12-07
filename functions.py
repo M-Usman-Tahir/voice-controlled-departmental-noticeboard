fb350b7f5abce99917b0a188595519cf75557627
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-12-07 21:52:15 +0500

    Updated: Voice engine auto import from textToSpeech

--- functions.py
+++ functions.py
@@ -11,31 +11,31 @@ from OpenFile import *
 from FaceDetection import * # import SysPaths
 
 
-def Command(engine, text):
+def Command(text):
     if IfIn(["log", "in"], text, "and") or "scan" in text:
         name = detectFace()
         if name != "Not Found":
             LogIN(name)
         else:
-            say(engine, "No matching face detected!")
+            say("No matching face detected!")
     if IfIn(["opportunity", "opportunities"], text):
         dirs = os.listdir(OpportunityPath)
         print(FunList(dirs))
-        say(engine, "Say the number of notification to open")
+        say("Say the number of notification to open")
         TEXT = speak(engine)
         N=getNum(TEXT)
         OPEN(os.path.join(OpportunityPath, dirs[N-1]))
     if IfIn(["public", "department"], text):
         dirs = os.listdir(DepartmentPath)
         print(FunList(dirs))
-        say(engine, "Say the number of notification to open")
+        say("Say the number of notification to open")
         TEXT = speak(engine)
         N=getNum(TEXT)
         OPEN(os.path.join(DepartmentPath, dirs[N-1]))
     if IfIn(["society", "societies"], text):
         dirs = os.listdir(SocietyPath)
         print(FunList(dirs))
-        say(engine, "Say the number of notification to open")
+        say("Say the number of notification to open")
         TEXT = speak(engine)
         N=getNum(TEXT)
         OPEN(os.path.join(SocietyPath, dirs[N-1]))
