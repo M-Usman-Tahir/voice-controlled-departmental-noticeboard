4db1f6819944cc68ddd185f355f7495f3897015a
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-12-11 12:49:05 +0500

    Updated: Packages' Node and API updation

--- functions.py
+++ functions.py
@@ -2,42 +2,9 @@
     Takes all funstions here for the main file]
 """
 
-from SysPaths import *
 from SetNoti import *
 from Speech import * # imports speech_recognition as sr
 from BOOT import * # imports pandas as pd, os, PasswordGenerator
-from BootMails import * # imports SendmMails
-from Login import *
-from OpenFile import *
-from FaceDetection import * # import SysPaths
+from Mails.BootMails import * # imports SendmMails
+from Authentication.FaceDetection import * # import SysPaths
 
-
-def Command(text):
-    if IfIn(["log", "in"], text, "and") or "scan" in text:
-        name = detectFace()
-        if name != "Not Found":
-            LogIN(name)
-        else:
-            say("No matching face detected!")
-    if IfIn(["opportunity", "opportunities"], text):
-        dirs = os.listdir(OpportunityPath)
-        print(FunList(dirs))
-        say("Say the number of notification to open")
-        TEXT = speak(engine)
-        N=getNum(TEXT)
-        OPEN(os.path.join(OpportunityPath, dirs[N-1]))
-    if IfIn(["public", "department"], text):
-        dirs = os.listdir(DepartmentPath)
-        print(FunList(dirs))
-        say("Say the number of notification to open")
-        TEXT = speak(engine)
-        N=getNum(TEXT)
-        OPEN(os.path.join(DepartmentPath, dirs[N-1]))
-    if IfIn(["society", "societies"], text):
-        dirs = os.listdir(SocietyPath)
-        print(FunList(dirs))
-        say("Say the number of notification to open")
-        TEXT = speak(engine)
-        N=getNum(TEXT)
-        OPEN(os.path.join(SocietyPath, dirs[N-1]))
-        
