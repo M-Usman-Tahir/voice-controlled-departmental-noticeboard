6d7e69fc6b7f371ba3e6819269d1157408dec2f6
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-12-12 15:39:35 +0500

    Fixed: Circular imports

---
+++ Commands.py
@@ -0,0 +1,60 @@
+
+from Authentication.Login import *
+from Authentication.FaceDetection import *
+           
+def IfIn(words, text, opr="or"):
+    """[It take a array of words and checks if the words are present in the text
+    and return their AND or OR relation mentioned in the arguments.]
+
+    Args:
+        words ([list of strings]): [The words to check if present in text]
+        text ([string]): [The simple string text]
+        opr (str, optional): [The logical realtion of booleans i.e. AND, OR]. Defaults to "or".
+
+    Returns:
+        [boolean]: [The relation of 'opr' with all the boolean of matches (word in text)]
+    """
+    if opr=="or":
+        for word in words:
+            if word in text:
+                return True
+        return False
+    for word in words:
+        if word in text:
+            continue
+        return False
+    return True
+
+# def AskCRE():
+#     name = input("")
+
+def Command(text):
+    if IfIn(["log", "in"], text, "and") or "scan" in text:
+        name = detectFace()
+        if name != "Not Found":
+            LogIN(name)
+        else:
+            say("No matching face detected!")
+            # AskCRE()
+    if IfIn(["opportunity", "opportunities"], text):
+        dirs = os.listdir(OpportunityPath)
+        print(FunList(dirs))
+        say("Say the number of notification to open")
+        TEXT = speak()
+        N=getNum(TEXT)
+        OPEN(os.path.join(OpportunityPath, dirs[N-1]))
+    if IfIn(["public", "department"], text):
+        dirs = os.listdir(DepartmentPath)
+        print(FunList(dirs))
+        say("Say the number of notification to open")
+        TEXT = speak()
+        N=getNum(TEXT)
+        OPEN(os.path.join(DepartmentPath, dirs[N-1]))
+    if IfIn(["society", "societies"], text):
+        dirs = os.listdir(SocietyPath)
+        print(FunList(dirs))
+        say("Say the number of notification to open")
+        TEXT = speak()
+        N=getNum(TEXT)
+        OPEN(os.path.join(SocietyPath, dirs[N-1]))
+        
