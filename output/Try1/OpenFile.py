0afaaee4f9b150d92d1c22e771177d188323bbf7
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2021-12-18 22:14:02 +0500

    Removed: Compiled exe

--- output/Try1/OpenFile.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def OPEN(filename):
-    """[Open the file as from cmd]
-
-    Args:
-        filename ([string]): [The name/path of the file to be opened]
-    """
-    os.system(f'"{filename}"')
