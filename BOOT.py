918f4d0297b496e93629f60679739241545fedb9
Muhammad Usman Tahir <m.usman.tahir.336@gmail.com>
2022-01-07 00:28:31 +0500

    Fixed: Bug

--- BOOT.py
+++ BOOT.py
@@ -10,48 +10,49 @@ def StructureBoot(part, check="Reg_No"):
     
     for root, dirs, files in os.walk(os.getcwd()):
         for name in files:
-            file1 = pd.read_csv(name)
-            NewPath = os.path.join(CurrentDir, "LOCAL", part)
-            NewPath = os.path.join(NewPath, "Session-{}".format(file1[check].iloc[0].split("-")[0])) if part=="Student" else NewPath
-            try:
-                os.mkdir(NewPath)
-            except FileExistsError:
-                pass
-            for i in file1[check]:
+            if ".csv" in name:
+                file1 = pd.read_csv(name)
+                NewPath = os.path.join(CurrentDir, "LOCAL", part)
+                NewPath = os.path.join(NewPath, "Session-{}".format(file1[check].iloc[0].split("-")[0])) if part=="Student" else NewPath
                 try:
-                    Email = list(file1[file1[check]==i]["Email"])[0]
-                    listMails.append(Email)
-                    os.mkdir(os.path.join(NewPath, i))
-                except:
-                    continue
-                try:
-                    if part == "Student":
-                        Name = list(file1[file1[check]==i]["Name"])[0]
-                        RollNum = i
-                        Password = PasswordGenerator()
-                        with open("{}.txt".format(os.path.join(NewPath, i, "CREs")), "w") as Cre:
-                            Data = "Name: {}\nRoll_Number: {}\nEmail: {}\nPasswaord: {}\n".format(Name, RollNum, Email, Password)
-                            Cre.write(DC(Data))
-                            # os.chdir(CurrentDir)
-                            # ! SendBootCre(Data, Email, Topic="Student")
-                            # os.chdir(NewPath)
-                            
-                        
-                    elif part == "Faculty":
-                        Name = i
+                    os.mkdir(NewPath)
+                except FileExistsError:
+                    pass
+                for i in file1[check]:
+                    try:
                         Email = list(file1[file1[check]==i]["Email"])[0]
-                        Position = list(file1[file1[check]==i]["Position"])[0]
-                        Password = PasswordGenerator()
-                        Subjects = list(file1[file1[check]==i]["Subjects"])[0]
-                        with open("{}.txt".format(os.path.join(NewPath, i, "CREs")), "w") as Cre:
-                            Data = "Name: {}\nPosition: {}\nEmail: {}\nPasswaord: {}\nSubjects: {}\n".format(Name, Position, Email, Password, Subjects)
-                            Cre.write(DC(Data))
-                            # os.chdir(CurrentDir)
-                            # ! SendBootCre(Data, Email, Topic="Teacher")
-                            # os.chdir(NewPath)
+                        listMails.append(Email)
+                        os.mkdir(os.path.join(NewPath, i))
+                    except:
+                        continue
+                    try:
+                        if part == "Student":
+                            Name = list(file1[file1[check]==i]["Name"])[0]
+                            RollNum = i
+                            Password = PasswordGenerator()
+                            with open("{}.txt".format(os.path.join(NewPath, i, "CREs")), "w") as Cre:
+                                Data = "Name: {}\nRoll_Number: {}\nEmail: {}\nPasswaord: {}\n".format(Name, RollNum, Email, Password)
+                                Cre.write(DC(Data))
+                                # os.chdir(CurrentDir)
+                                # ! SendBootCre(Data, Email, Topic="Student")
+                                # os.chdir(NewPath)
+                                
                             
-                except Exception as e:
-                    print(i, e)                 
+                        elif part == "Faculty":
+                            Name = i
+                            Email = list(file1[file1[check]==i]["Email"])[0]
+                            Position = list(file1[file1[check]==i]["Position"])[0]
+                            Password = PasswordGenerator()
+                            Subjects = list(file1[file1[check]==i]["Subjects"])[0]
+                            with open("{}.txt".format(os.path.join(NewPath, i, "CREs")), "w") as Cre:
+                                Data = "Name: {}\nPosition: {}\nEmail: {}\nPasswaord: {}\nSubjects: {}\n".format(Name, Position, Email, Password, Subjects)
+                                Cre.write(DC(Data))
+                                # os.chdir(CurrentDir)
+                                # ! SendBootCre(Data, Email, Topic="Teacher")
+                                # os.chdir(NewPath)
+                                
+                    except Exception as e:
+                        print(i, e)                 
         break
     os.chdir(CurrentDir)
 
